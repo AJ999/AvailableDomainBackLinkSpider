9015d69d1dec44f8a91914fe0ba14ca5fa3b88f9
andrew.jack999 <andrew.jack@euromoneyplc.com>
2015-10-09 17:05:46 +0100

    Adding Web and Test Projects

---
+++ AvailableDomainBackLinkSpider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AvailableDomainBackLinkSpider.Startup))]
+namespace AvailableDomainBackLinkSpider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
